8511e58cbf58de82d60364d6aaa5ceb552bdeea9
Abhay Singh <abhaysingh03032002@gmail.com>
2026-09-16 01:58:45 +0530

    first python code

---
+++ python/basic/Hello.py
@@ -0,0 +1,12 @@
+print("Hello python")
+
+# In same line
+print("Hello Python" ,"My name is Abhay")
+
+#for break a line -\n -> special character
+print("Hello Python \n It is lovely to meet you ," ,"My name is Abhay Singh :)" )
+
+# output
+
+# Hello Python 
+#  It is lovely to meet you , My name is Abhay Singh :) 
